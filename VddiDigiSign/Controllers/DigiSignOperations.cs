d96a8644d5584e2d32b90c02766292b893870bbf
riyaaditmonkey <riyaad.salasa@itmonkey.co.za>
2020-10-24 16:35:36 +0200

    Fixed Delete

---
+++ VddiDigiSign/Controllers/DigiSignOperations.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VddiDigiSign.Controllers
+{
+    public class DigiSignOperations
+    {
+        //LoggingOperations logOps = new LoggingOperations();
+
+        public Models.DigiSignature loadSignature(string residentId)
+        {
+            var db = new PetaPoco.Database("Olgarsconnection");
+
+            Models.DigiSignature ApplicationSignatureDetail = new Models.DigiSignature();
+            ApplicationSignatureDetail = db.SingleOrDefault<Models.DigiSignature>("select * from DigiSignature where ResidentId = @0", residentId);
+            return ApplicationSignatureDetail;
+        }
+
+        public int checkSignature(string residentId)
+        {
+            int checkLead;
+            //Just checked for the Filter
+            var db = new PetaPoco.Database("Olgarsconnection");
+
+            checkLead = db.ExecuteScalar<int>("SELECT Count(*) FROM DigiSignature where ResidentId=@0", residentId);
+
+            return checkLead;
+            }
+
+         public int getSignatureId(string residentId)
+        {
+            int latestSignatureId = 0;
+
+            string leadCheck = "";
+
+            leadCheck = "SELECT Max(DigiSignatureId) FROM [DigiSignature] WHERE [ResidentId]=@0";
+
+            var db = new PetaPoco.Database("Olgarsconnection");
+            latestSignatureId = db.ExecuteScalar<int>(leadCheck, residentId);
+            return latestSignatureId;
+        }
+
+        public int addSignature(Models.DigiSignature currentSignatureObject)
+        {
+
+            int currentObjectId = -1;
+            var db = new PetaPoco.Database("Olgarsconnection");
+            db.Insert("DigiSignature", "DigiSignatureId", true, currentSignatureObject);
+            currentObjectId = currentSignatureObject.DigiSignatureId;
+            return currentObjectId;
+
+        }
+
+        public int DeleteSignature(string residentId)
+        {
+            int sigDeleteId = 0;
+
+            var db = new PetaPoco.Database("Olgarsconnection");
+
+            sigDeleteId = db.Execute("delete from DigiSignature where ResidentId =  @0 ", residentId);
+
+            return sigDeleteId;
+
+        }
+
+        public void updateDigiSignature(Models.DigiSignature currentUserSignature)
+        {
+
+            var db = new PetaPoco.Database("Olgarsconnection");
+            db.Update("DigiSignature", "DigiSignatureId", currentUserSignature, new string[] { "ResidentInitial", "ResidentSignature", "SalesInitial", "SalesSignature", "WitnessInitial", "WitnessSignature", "WitnessName" });
+
+        }
+
+
+
+    }
+}
---
+++ VddiDigiSign/Models/DigiSignature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VddiDigiSign.Models
+{
+    public class DigiSignature
+    {
+        public int DigiSignatureId { get; set; }
+        public string ResidentId { get; set; }
+        public string ResidentInitial { get; set; }
+        public string ResidentSignature { get; set; }
+        public string SalesInitial { get; set; }
+        public string SalesSignature { get; set; }
+        public string WitnessInitial { get; set; }
+        public string WitnessSignature { get; set; }
+        public string WitnessName { get; set; }
+        public string AddedBy { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
